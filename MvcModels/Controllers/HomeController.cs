e1d133efea6edf86ba35eb37be7071c3879060df
srkneo <yadavmaheshing@gmail.com>
2016-02-11 12:58:06 +0530

    Max

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcModels.Models;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private Person[] personData = {
+            new Person {PersonId = 1, FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+            new Person {PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User},
+            new Person {PersonId = 3, FirstName = "John", LastName = "Smith", Role = Role.User},
+            new Person {PersonId = 4, FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+        };
+
+        public ActionResult Index(int id = 1)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+
+        public ActionResult DisplaySummary([Bind(Prefix="HomeAddress")]AddressSummary summary)
+        {
+            return View(summary);
+        }
+	}
+}
